793f91a8f505da26cac2017013699608a914b69f
Melanny Burkoot <melannyburkoot@hotmail.com>
2023-08-10 16:28:50 -0300

    Criação de projeto

--- cypress/support/gui_commands.js
+++ cypress/support/gui_commands.js
@@ -1,6 +1,7 @@
 Cypress.Commands.add('login', (
     user = Cypress.env('user_name'),
     password = Cypress.env('user_password'),
+    { cacheSession = true } = {},
   ) => {
     const login = () => {
       cy.visit('/users/sign_in')
@@ -10,7 +11,22 @@ Cypress.Commands.add('login', (
       cy.get("[data-qa-selector='sign_in_button']").click()
     }
   
-    login()
+    const validate = () => {
+      cy.visit('/')
+      cy.location('pathname', { timeout: 1000 })
+        .should('not.eq', '/users/sign_in')
+    }
+  
+    const options = {
+      cacheAcrossSpecs: true,
+      validate,
+    }
+  
+    if (cacheSession) {
+      cy.session(user, login, options)
+    } else {
+      login()
+    }
   })
   
   Cypress.Commands.add('logout', () => {
@@ -18,5 +34,13 @@ Cypress.Commands.add('login', (
     cy.get('.qa-user-avatar').click()
     cy.contains('Sign out').click()
       
- 
+  })
+
+  Cypress.Commands.add('gui_createProject', project => {
+    cy.visit('/projects/new')
+  
+    cy.get('#project_name').type(project.name)
+    cy.get('#project_description').type(project.description)
+    cy.get('.qa-initialize-with-readme-checkbox').check()
+    cy.contains('Create project').click()
   })
